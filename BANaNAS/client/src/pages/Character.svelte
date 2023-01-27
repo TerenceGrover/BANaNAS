c97704cd0bf5c98bf70c2f7005197831d283341f
alexryanjones <alexryanjones@gmx.com>
2023-01-27 19:40:39 +0100

    add: category list

--- BANaNAS/client/src/pages/Character.svelte
+++ BANaNAS/client/src/pages/Character.svelte
@@ -1,16 +1,22 @@
 <script>
   import Loader_1 from "../components/Loader-1.svelte";
+  import Category_Collection from "../components/Category-Collection.svelte";
   export let changePage;
+
+  let selectedCategory = {name: ''};
 
 </script>
 
 <main>
 
   <h1>Character</h1>
+  <h2>{selectedCategory.name}</h2>
+
   <button on:click={() => changePage('home')}>Back</button>
   <button on:click={() => changePage('data')}>FIGHT</button>
 
   <Loader_1 />
+  <Category_Collection bind:currentlySelected={selectedCategory}/>
 
 </main>
 
