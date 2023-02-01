7abee80fbf2739fe1b84ae4dc8dee4680bb1323d
TerenceGrover <terencegrover@gmail.com>
2023-02-01 13:44:23 +0100

    fix: utils to Utils in Scatterplot

--- client/src/components/Data/Scatterplot.svelte
+++ client/src/components/Data/Scatterplot.svelte
@@ -1,7 +1,7 @@
 <script>
   import { onMount } from "svelte";
   import * as d3 from "d3";
-  import {splitWordsOnCapitalLetters} from '../../utils/helpers';
+  import {splitWordsOnCapitalLetters} from '../../Utils/helpers';
 
   export let data1;
   export let data2;
