f87863d65bdc716b217ee1a6aa1df0b9f9a232e0
TerenceGrover <terencegrover@gmail.com>
2023-02-01 14:19:30 +0100

    feat: BAckground for mobile character screen

--- client/src/components/Char-Animated-Background.svelte
+++ client/src/components/Char-Animated-Background.svelte
@@ -1,29 +1,38 @@
 <script>
+  import { onMount } from 'svelte';
 
-  import { onMount } from "svelte";
+  const isMobile = window.innerWidth < 768;
 
   onMount(() => {
-    setTimeout(() => {
-      document.getElementById('left').style.left = '0';
-      document.getElementById('left-under').style.left = '10px';
-      document.getElementById('right').style.right = '0';
-      console.log('added')
-    }, 1500)
-  })
+    !isMobile
+      ? setTimeout(() => {
+          document.getElementById('left').style.left = '0';
+          document.getElementById('left-under').style.left = '10px';
+          document.getElementById('right').style.right = '0';
+        }, 1500)
+      : setTimeout(() => {
+          console.log('mobile');
+          document.getElementById('top-mobile').style.left = '0';
+          document.getElementById('bot-mobile').style.right = '0';
+          document.getElementById('bot-mobile').style.bottom = '0';
+          document.getElementById('top-mobile').style.top = '0';
 
+        }, 1500);
+  });
 </script>
 
 <main>
-
-  <div id='left' class="backgound-element"></div>
-  <div id='left-under' class="backgound-element"></div>
-  <div id='right' class="backgound-element"></div>
-
-
+  {#if isMobile}
+    <div id="top-mobile" class="mobile-element" />
+    <div id="bot-mobile" class="mobile-element" />
+  {:else}
+    <div id="left" class="backgound-element" />
+    <div id="left-under" class="backgound-element" />
+    <div id="right" class="backgound-element" />
+  {/if}
 </main>
 
 <style>
-
   main {
     position: absolute;
     height: 100vh;
@@ -41,22 +50,22 @@
   }
 
   #left-under {
-  background: #fff;
-  position: absolute;
-  left: -99.5vw;
-  top: 10px;
-  clip-path: polygon(80% 0, 60% 50%, 41% 50%, 20% 100%, 0 100%, 0 0);
-  z-index: 8;
+    background: #fff;
+    position: absolute;
+    left: -99.5vw;
+    top: 10px;
+    clip-path: polygon(80% 0, 60% 50%, 41% 50%, 20% 100%, 0 100%, 0 0);
+    z-index: 8;
   }
 
-  #left-under:before{
-    content : '';
+  #left-under:before {
+    content: '';
     background: #000000aa;
     height: 100vh;
     width: 100vw;
     display: block;
     position: absolute;
-    left: -.25vw;
+    left: -0.25vw;
     top: -5px;
     clip-path: polygon(80% 0, 60% 50%, 41% 50%, 20% 100%, 0 100%, 0 0);
   }
@@ -69,13 +78,36 @@
     z-index: 9;
   }
 
-  .backgound-element{
+  .backgound-element {
     position: absolute;
     top: 0;
-    transition: all 1.8s cubic-bezier(0.77,0,0.26,1.01);
+    transition: all 1.8s cubic-bezier(0.77, 0, 0.26, 1.01);
     width: 100vw;
     height: 100vh;
   }
 
+  #top-mobile {
+    background-color: #052c46;
+    position: absolute;
+    left: -100vw;
+    top: -5vh;
+    clip-path: polygon(0 0, 100% 0, 100% 50%, 40% 45%, 60% 55%, 0 50%);
+    z-index: 11;
+  }
 
+  #bot-mobile {
+    background-color: #fed703;
+    position: absolute;
+    right: -100vw;
+    bottom: -5vh;
+    clip-path: polygon(0 100%, 100% 100%, 100% 50%, 40% 45%, 60% 55%, 0 50%);
+    z-index: 9;
+  }
+
+  .mobile-element {
+    position: absolute;
+    transition: all 1.8s cubic-bezier(0.77, 0, 0.26, 1.01);
+    width: 100vw;
+    height: 100vh;
+  }
 </style>
