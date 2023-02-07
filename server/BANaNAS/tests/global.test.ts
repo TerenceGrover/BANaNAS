b8a222f805c778acff5d3d909eaae602a6fc719e
Al366io <alessio.nannipieri0@gmail.com>
2023-02-07 11:09:24 +0100

    tests: added complete tests

--- server/BANaNAS/tests/global.test.ts
+++ server/BANaNAS/tests/global.test.ts
@@ -1,38 +1,41 @@
-import { it } from 'node:test';
 import request from 'supertest';
-import app from '../src/SERVER_BANaNAS';
+import app from '../dist/SERVER_BANaNAS';
 
-let server;
-function startServer(){
-  return new Promise((resolve) => {
-    server = http.createServer(app);
-    server.listen(3000, () => {
-      resolve(server);
-    });
-  });
-}
-
-function stopServer(){
-  return new Promise((resolve) => {
-    server.close(() => {
-      resolve(server);
-    });
-  });
-}
-
-describe('login & register functionalities', () => {
-
-  beforeAll(async () => {
-    await startServer();
-  });
-
-  afterAll(async () => {
-    await stopServer();
-  });
-
+describe('banana private api test', () => {
   it('should respond with a random banana fact', async () => {
     const res = await request(app).get('/bananaFact');
     expect(res.status).toBe(200);
     expect(res.body).toHaveProperty('fact');
+  });
+
+  it('should get the number of bananas eaten', async () => {
+    const res = await request(app).get('/bananas');
+    expect(res.status).toBe(200);
+    const { total } = res.body;
+    expect(total).toBeGreaterThanOrEqual(0);
+  });
+});
+
+describe('random calls to outside API', () => {
+  it('should return all the metrics for one category', async () => {
+    const res = await request(app).get('/api/Agriculture');
+    expect(res.status).toBe(200);
+    const { description, parameters_needed, available_countries } = res.body.AgriculturalIrrigatedLand;
+    expect(description).toBeDefined();
+    expect(parameters_needed).toBeDefined();
+    expect(available_countries).toBeDefined();
+    
+  });
+  it('should return the description for a selected metric', async () => {
+    const res = await request(app).get('/api/Agriculture/ForestArea');
+    expect(res.status).toBe(200);
+    const { description } = res.body;
+    expect(description).toBeDefined();
+  });
+  it('should correctly call an endpoint and get the data', async () => {
+    const res = await request(app).get('/api/Agriculture/ForestArea/Italy/2010/2015');
+    expect(res.status).toBe(200);
+    console.log(res.body);
+    expect(res.body['2014']).toBeDefined();
   });
 });
